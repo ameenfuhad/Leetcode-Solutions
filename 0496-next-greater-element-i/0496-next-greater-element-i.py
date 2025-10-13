c1a4e9ab3ec881a861bf538ca09cd96dc4fad47a
Ameen Fuhad <ameenfuhadkm123@gmail.com>
2025-10-13 10:40:13 +0530

    Time: 41 ms (21.35%), Space: 12.5 MB (98.99%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -3,8 +3,7 @@ class Solution(object):
         r=[]
         for i in nums1:
             y=-1
-            l=nums2.index(i)
-            for k in range(l+1,len(nums2)):
+            for k in range(nums2.index(i)+1,len(nums2)):
                 if nums2[k] > i :
                     y=nums2[k]
                     break
