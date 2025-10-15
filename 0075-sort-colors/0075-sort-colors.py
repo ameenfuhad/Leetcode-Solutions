4b251530fbc0a830aff45978b9134f7bcde69203
Ameen Fuhad <ameenfuhadkm123@gmail.com>
2025-10-15 20:20:44 +0530

    Time: 3 ms (20.95%), Space: 12.4 MB (52.4%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def sortColors(self, nums):
+        for i in range(len(nums)-1):
+            for j in range(i+1,len(nums)):
+                if(nums[i]>nums[j]):
+                    a=nums[i]
+                    nums[i]=nums[j]
+                    nums[j]=a
+        return nums
+        
