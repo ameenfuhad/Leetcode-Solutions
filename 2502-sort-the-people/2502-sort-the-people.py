c14378d3b83a8dcd28fdb993169d106377c3f91e
Ameen Fuhad <ameenfuhadkm123@gmail.com>
2025-10-17 23:36:38 +0530

    Time: 806 ms (8.06%), Space: 12.8 MB (11.64%) - LeetHub

--- 2502-sort-the-people/2502-sort-the-people.py
+++ 2502-sort-the-people/2502-sort-the-people.py
@@ -3,11 +3,7 @@ class Solution(object):
         for i in range(len(heights)-1):
             for j in range(i+1,len(heights)):
                 if heights[i]<heights[j]:
-                    a=heights[i]
-                    heights[i]=heights[j]
-                    heights[j]=a
-                    a=names[i]
-                    names[i]=names[j]
-                    names[j]=a
+                    heights[i],heights[j]=heights[j],heights[i]
+                    names[i],names[j]=names[j],names[i]
         return names
         
