cbfdc93f870b692ce011fe7b2fb63eb1bd0695bb
Ameen Fuhad <ameenfuhadkm123@gmail.com>
2025-10-18 12:34:27 +0530

    Time: 3 ms (62.28%), Space: 12.5 MB (78.59%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,8 +1,6 @@
 class Solution(object):
     def findMedianSortedArrays(self, nums1, nums2):
-        for i in nums2:
-            nums1.append(i)
-        nums1=sorted(nums1)
+        nums1=sorted(nums1+nums2)
         l=len(nums1)
         if(l%2==0):
             return (nums1[l//2]+nums1[l//2-1])/2.0
