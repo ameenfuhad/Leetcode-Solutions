7444a09749235126022cb973536b455db1541288
Ameen Fuhad <ameenfuhadkm123@gmail.com>
2025-10-18 18:00:41 +0530

    Time: 18 ms (30.65%), Space: 13.2 MB (89.51%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def missingNumber(self, nums):
+        nums=sorted(nums)
+        for i in range(0,len(nums)):
+            if(nums[i]==i):
+                continue
+            else:
+                return i
+        return len(nums)
+        
