9e8b895d58c0b9a1666ef0de4a4d4e7499ab24ec
Ameen Fuhad <ameenfuhadkm123@gmail.com>
2025-10-20 11:42:58 +0530

    Time: 4 ms (15.38%), Space: 12.4 MB (82.34%) - LeetHub

--- 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.py
+++ 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.py
@@ -4,9 +4,7 @@ class Solution(object):
         for i in operations:
             if(i=="X++" or i=="++X"):
                 x=x+1
-            elif(i=="X--" or i=="--X"):
-                x=x-1
             else:
-                continue
+                x=x-1
         return x
         
