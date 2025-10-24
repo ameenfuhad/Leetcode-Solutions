ec17b20adfdff5fa24d31bce782b6f3bff3a436a
Ameen Fuhad <ameenfuhadkm123@gmail.com>
2025-10-24 22:18:25 +0530

    Time: 0 ms (100%), Space: 12.4 MB (81.92%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def isPowerOfFour(self, n):
+        while(n%4==0 and n!=0):
+            n=n/4
+        if(n==1):
+            return True
+        else:
+            return False
