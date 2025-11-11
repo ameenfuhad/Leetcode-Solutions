bc99b35edb75d4ab33d206ca1785ed4d16498916
Ameen Fuhad <ameenfuhadkm123@gmail.com>
2025-11-11 18:14:27 +0530

    Time: 0 ms (100%), Space: 12.4 MB (79.61%) - LeetHub

--- 0782-jewels-and-stones/0782-jewels-and-stones.py
+++ 0782-jewels-and-stones/0782-jewels-and-stones.py
@@ -2,8 +2,6 @@ class Solution(object):
     def numJewelsInStones(self, jewels, stones):
         c=0
         for i in jewels:
-            for j in stones:
-                if i==j:
-                    c+=1
-        return c
-        
+            o=stones.count(i)
+            c+=o
+        return c     
