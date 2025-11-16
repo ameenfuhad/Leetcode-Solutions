934201b2f9d41cd0ec3a3b260573457fdd53162b
Ameen Fuhad <ameenfuhadkm123@gmail.com>
2025-11-16 14:57:51 +0530

    Time: 15 ms (76.01%), Space: 13.6 MB (69.83%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
@@ -3,7 +3,7 @@ class Solution(object):
         s=list(s)
         f=0
         l=len(s)-1
-        while(f!=l and f<l):
+        while(f<l):
             if s[f] in 'aeiouAEIOU':
                 if s[l] in 'aeiouAEIOU':
                     s[l],s[f]=s[f],s[l]
