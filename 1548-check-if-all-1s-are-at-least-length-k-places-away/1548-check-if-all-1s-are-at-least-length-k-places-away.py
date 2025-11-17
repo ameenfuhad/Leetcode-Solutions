ee4452e7e671b15dbeb53fb2bc93b8dfe52522eb
Ameen Fuhad <ameenfuhadkm123@gmail.com>
2025-11-17 09:25:01 +0530

    Time: 8 ms (75%), Space: 16.3 MB (48.08%) - LeetHub

--- 1548-check-if-all-1s-are-at-least-length-k-places-away/1548-check-if-all-1s-are-at-least-length-k-places-away.py
+++ 1548-check-if-all-1s-are-at-least-length-k-places-away/1548-check-if-all-1s-are-at-least-length-k-places-away.py
@@ -2,10 +2,11 @@ class Solution(object):
     def kLengthApart(self, nums, k):
         if 1 in nums:
             i=nums.index(1)
+            l=len(nums)-1-(nums[::-1].index(1))
         else:
             return True
         c=0
-        for i in range(i+1,len(nums)):
+        for i in range(i+1,l+1):
             if(nums[i]==1):
                 if(c>=k):
                     c=0
