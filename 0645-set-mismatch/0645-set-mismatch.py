111d150e621a2edb944ce3aa8292e7e300513194
Ameen Fuhad <ameenfuhadkm123@gmail.com>
2025-11-20 09:33:31 +0530

    Time: 5715 ms (5.55%), Space: 13.4 MB (89%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.py
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -3,11 +3,10 @@ class Solution(object):
         l=[]
         miss=0
         nums.sort()
-        for i in range(len(nums)):
-            if i+1 not in nums:
-                miss=i+1
-            if i!=len(nums)-1:
-                if(nums[i]==nums[i+1]):
-                    l.append(nums[i])
-        l.append(miss)
+        for i in range(len(nums)-1):
+            if(nums[i]==nums[i+1]):
+                l.append(nums[i])
+        for i in range(1,len(nums)+1):
+            if i not in nums:
+                l.append(i)
         return l
